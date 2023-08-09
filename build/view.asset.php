bb33a212d44242e7255b3d7c081092086587e5c0
Ross Morsali <ross@weareelevate.com>
2023-08-09 17:41:06 +0200

    Run create-block

---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '42058a0fc4863644ed87');
---
+++ src/view.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+console.log( 'Hello World! (from create-block-reveal-post-data block)' );
+/* eslint-enable no-console */
