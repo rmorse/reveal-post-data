07499b831268be1e9f940e3d9a7514cebc8e1358
Ross Morsali <ross@codeamp.io>
2023-11-11 11:44:51 +0100

    Prevent direct access to the plugin file Update readme and version number

--- readme.txt
+++ readme.txt
@@ -12,6 +12,8 @@ Adds a button in the editor sidebar to show the raw post data.
 
 Adds a button in the editor sidebar to show the raw post data - useful for debugging.
 
+Checkout the [GitHub repo](https://github.com/rmorse/reveal-post-data) for more information.
+
 == Installation ==
 
 == Frequently Asked Questions ==
@@ -20,6 +22,9 @@ Adds a button in the editor sidebar to show the raw post data - useful for debug
 
 == Changelog ==
 
+= 0.1.1 =
+- Prevent direct access to plugin file
+
 = 0.1.0 =
-* Release
+- Release
 
--- reveal-post-data.php
+++ reveal-post-data.php
@@ -4,14 +4,17 @@
  * Description:       Adds a button in the editor sidebar to show the raw post data.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Ross Morsali
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       reveal-post-data
- *
- * @package           create-block
  */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; 
+}
 
 /**
  * Load admin JS + CSS files.
