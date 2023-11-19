9a0f82dc39c81d6444b1c3b53c77d2a44a48a0f0
Ross Morsali <ross@codeamp.io>
2023-11-19 15:28:50 +0100

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'b4e11208206dbcb7a1a4');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '6cd7082a919a7dca42b8');
